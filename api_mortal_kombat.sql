d0c55717a34e2233ea1d23b286b025edf9ab51b8
Lucas-Ortlieb <105297070+Lucas-Ortlieb@users.noreply.github.com>
2023-05-08 21:23:35 -0300

    Add files via upload

--- api_mortal_kombat.sql
+++ api_mortal_kombat.sql
@@ -2,9 +2,16 @@ create database if not exists api_mortal_kombat;
 
 use api_mortal_kombat;
 
+create table if not exists usuarios(
+id int primary key auto_increment,
+nome_usuario varchar(30) not null unique key,
+senha varchar(15) not null,
+email varchar(50) not null unique key
+);
+
 create table if not exists arenas(
 id tinyint primary key auto_increment,
-nome_arena varchar(30) not null,
+nome_arena varchar(30) not null unique key,
 localizacao varchar(30) not null,
 aparencia varchar(30) not null,
 aparicoes varchar(30) not null
@@ -12,22 +19,18 @@ aparicoes varchar(30) not null
 
 create table if not exists personagens(
 id tinyint primary key auto_increment,
-nome_personagem varchar(30) not null unique key,
+nome_personagem varchar(30) not null,
 genero varchar(15) not null,
 especie varchar(20) not null,
 estilo_de_luta varchar(20) not null,
 idade smallint not null, 
-arma varchar(20) not null,
-arena_id tinyint not null,
-constraint fk_arena_id foreign key (arena_id) references arenas(id) 
+arma varchar(20) not null
 );
 
-create table if not exists usuarios(
-nome_usuario varchar(30) primary key not null unique key,
-senha varchar(15) not null,
-email varchar(50) not null unique key,
-arena_id tinyint not null,
+create table if not exists personagens_arenas(
+arenas_id tinyint not null,
 personagens_id tinyint not null,
-constraint fk2_arena_id foreign key (arena_id) references arenas(id),
-constraint fk_personagens_id foreign key (personagens_id) references personagens(id)  
+constraint fk_arena foreign key (arenas_id) references arenas(id),
+constraint fk_personagem foreign key (personagens_id) references personagens(id)
 );
+
